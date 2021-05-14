5ca192e96988d7f3c28e8334e6fe29309c1832f1
musicluvr <71690662+musicluvr@users.noreply.github.com>
2021-05-14 16:56:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,5 +4,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel", url="https://t.me/InFoTel_Group")],
+        [InlineKeyboardButton(
+            "Report Bugs 😊", url="https://t.me/InFoTel_Group")]
+    ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is a Telegram YouTube Download Bot!\n\n<b>Please send me any YouTube Link, I can upload to telegram as File/Video</b>\n\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
