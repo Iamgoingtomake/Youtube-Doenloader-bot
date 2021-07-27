635611867dbf0527f46c37b1029f19862282d227
Stalin John <71690662+musicluvr@users.noreply.github.com>
2021-07-27 10:33:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("UPDATES CHANNEL", url="https://t.me/EagleoBotUpdates")],
+        [InlineKeyboardButton("UPDATES CHANNEL", url="https://t.me/FoxBotUpdates")],
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is a Telegram YouTube Download Bot!\n\n<b>Please send me any YouTube Link, I can upload to telegram as File/Video</b>\n\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
